85c2a68a68cccf7a72590d1c001838fcd69f8bf5
KimMaynard <70479661+KimMaynard@users.noreply.github.com>
2025-04-03 00:39:24 -0400

    created jsx file for chp1 and css file to go w/ it

---
+++ certificate-app/src/components/chapter1.jsx
@@ -0,0 +1,23 @@
+import './styling/chapter1.css';
+
+const chapter1 = () => {
+    return (
+      <div>
+        <header>
+          <h1>What is an API</h1>
+        </header>
+        <p>
+          API stands for “Application Programming Interface.” An API is like a waiter at a restaurant. 
+          Imagine you're sitting at a table, and you tell the waiter, "I want a pizza." 
+          The waiter goes to the kitchen (where the food is made) and tells the chef what you want. 
+          When the pizza is ready, the waiter brings it back to your table. 
+          An API works the same way! 
+          If you type "cat pictures" on your computer, the API takes your request (like the waiter), 
+          asks a computer server (like the kitchen) for cat pictures, and then brings those pictures back to you. 
+          It's like magic, but with computers!
+        </p>
+      </div>
+    );
+  };
+  
+  export default chapter1;
