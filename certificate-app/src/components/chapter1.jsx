e3e1bf3ed71eb4431d87f87d9c8a396a4ff82a53
KimMaynard <70479661+KimMaynard@users.noreply.github.com>
2025-04-04 22:49:59 -0400

    added chp1 content

--- certificate-app/src/components/chapter1.jsx
+++ certificate-app/src/components/chapter1.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Quiz from './Quiz';
 import './Quiz.css';
-
+import './chapter1.css'
 
 const chapter1Questions = [
   {
@@ -24,7 +24,19 @@ const Chapter1 = () => {
 
   return (
     <div>
-      <h1>Chapter 1 Content</h1>
+      <header>
+        <h1>What is an API</h1>
+      </header>
+      <p className="API-explanation">
+        API stands for “Application Programming Interface.” An API is like a waiter at a restaurant.
+        Imagine you're sitting at a table, and you tell the waiter, "I want a pizza." 
+        The waiter goes to the kitchen (where the food is made) and tells the chef what you want. 
+        When the pizza is ready, the waiter brings it back to your table. 
+        An API works the same way! 
+        If you type "cat pictures" on your computer, the API takes your request (like the waiter), 
+        asks a computer server (like the kitchen) for cat pictures, and then brings those pictures back to you. 
+        It's like magic, but with computers!
+      </p>
       <Quiz questions={chapter1Questions} onFinish={handleQuizFinish} />
     </div>
   );
