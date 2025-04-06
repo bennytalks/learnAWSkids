2292bb6c0b4cc7ee2ec2a243106e8a8ad4c53481
KimMaynard <70479661+KimMaynard@users.noreply.github.com>
2025-04-05 23:52:16 -0400

    added chp3 content

--- certificate-app/src/components/Chapter3.jsx
+++ certificate-app/src/components/Chapter3.jsx
@@ -2,8 +2,35 @@ import React from 'react';
 
 const Chapter3 = () => {
   return (
-    <div>
-      <h2>content for chapter 3</h2>
+    <div className="AppInfo">
+      <header>
+        <h1>Apps</h1>
+      </header>
+
+      <div className="section Native-explanation">
+        <h3 className="Native-header">Native Apps:</h3>
+        These are apps that you download from the App Store or Google Play, like Angry Birds or YouTube...
+      </div>
+
+      <div className="section Web-explanation">
+        <h3 className="Web-header">Web Apps:</h3>
+        These apps you don’t need to download...
+      </div>
+
+      <div className="section Hybrid-explanation">
+        <h3 className="Hybrid-header">Hybrid Apps:</h3>
+        These are like a mix of the other two...
+      </div>
+
+      <div className="section Microservices-explanation">
+        <h3 className="Microservices-header">Microservices:</h3>
+        Imagine you have a big store like Costco...
+      </div>
+
+      <div className="section Webhooks-explanation">
+        <h3 className="Webhooks-header">Webhooks:</h3>
+        A webhook is like when your friend sends you a message...
+      </div>
     </div>
   );
 };
