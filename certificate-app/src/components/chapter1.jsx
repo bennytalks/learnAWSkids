f3c01f308f3969c330f1e45f9d583130a312226f
KimMaynard <70479661+KimMaynard@users.noreply.github.com>
2025-04-06 22:57:00 -0400

    Adjusted chp1 jsx to make it more aesthetic

--- certificate-app/src/components/chapter1.jsx
+++ certificate-app/src/components/chapter1.jsx
@@ -22,6 +22,7 @@ const Chapter1 = () => {
     // Save score to state, or send to Lambda later
   };
 
+  
   return (
     <div>
       <header>
@@ -29,7 +30,7 @@ const Chapter1 = () => {
       </header>
       <p className="API-explanation">
         API stands for “Application Programming Interface.” An API is like a waiter at a restaurant.
-        Imagine you're sitting at a table, and you tell the waiter, "I want a pizza." 
+        Imagine you're sitting at a table, and you tell the waiter, "I want a pizza."
         The waiter goes to the kitchen (where the food is made) and tells the chef what you want. 
         When the pizza is ready, the waiter brings it back to your table. 
         An API works the same way! 
@@ -37,6 +38,12 @@ const Chapter1 = () => {
         asks a computer server (like the kitchen) for cat pictures, and then brings those pictures back to you. 
         It's like magic, but with computers!
       </p>
+
+      <div className="image-container">
+        <img src="imgs/waiter.png" alt="waiter" className="Waiter-image" />
+        <img src="imgs/menu.png" alt="People looking at menu" className="Menu-image" />
+        <img src="imgs/pizza.png" alt="Pizza" className="Pizza-image" />
+      </div>
       <Quiz questions={chapter1Questions} onFinish={handleQuizFinish} />
     </div>
   );
