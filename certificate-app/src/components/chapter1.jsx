cde27e0400288285755c1e6e27b642e499fd3125
KimMaynard <70479661+KimMaynard@users.noreply.github.com>
2025-04-07 12:22:16 -0400

    trying to fix chp1 format

--- certificate-app/src/components/chapter1.jsx
+++ certificate-app/src/components/chapter1.jsx
@@ -24,28 +24,27 @@ const Chapter1 = () => {
 
   
   return (
-    <div>
-      <header>
-        <h1>What is an API</h1>
-      </header>
-      <p className="API-explanation">
-        API stands for “Application Programming Interface.” An API is like a waiter at a restaurant.
-        Imagine you're sitting at a table, and you tell the waiter, "I want a pizza."
-        The waiter goes to the kitchen (where the food is made) and tells the chef what you want. 
-        When the pizza is ready, the waiter brings it back to your table. 
-        An API works the same way! 
-        If you type "cat pictures" on your computer, the API takes your request (like the waiter), 
-        asks a computer server (like the kitchen) for cat pictures, and then brings those pictures back to you. 
-        It's like magic, but with computers!
-      </p>
-
-      <div className="image-container">
-        <img src="imgs/waiter.png" alt="waiter" className="Waiter-image" />
-        <img src="imgs/menu.png" alt="People looking at menu" className="Menu-image" />
-        <img src="imgs/pizza.png" alt="Pizza" className="Pizza-image" />
-      </div>
-      <Quiz questions={chapter1Questions} onFinish={handleQuizFinish} />
-    </div>
+    <body>
+  <header>
+    <h1>What is an API</h1>
+  </header>
+  <p className="API-explanation">
+    API stands for “Application Programming Interface.” An API is like a waiter at a restaurant.
+    Imagine you're sitting at a table, and you tell the waiter, "I want a pizza." 
+    The waiter goes to the kitchen (where the food is made) and tells the chef what you want. 
+    When the pizza is ready, the waiter brings it back to your table. 
+    An API works the same way! 
+    If you type "cat pictures" on your computer, the API takes your request (like the waiter), 
+    asks a computer server (like the kitchen) for cat pictures, and then brings those pictures back to you. 
+    It's like magic, but with computers!
+  </p>
+  
+  <div className="Image-container">
+    <img src="imgs/waiter.png" alt="waiter" className="Waiter-image" />
+    <img src="imgs/menu.png" alt="People looking at menu" className="Menu-image" />
+    <img src="imgs/pizza.png" alt="Pizza" className="Pizza-image" />
+  </div>
+</body>
   );
 };
 
